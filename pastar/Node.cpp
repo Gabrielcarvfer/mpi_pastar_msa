529a556e7561397ae5f2ce4846bf43c6f688876e
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2016-06-20 20:07:45 -0300

    pequenas modificações na alocação de custos dos nós

--- pastar/Node.cpp
+++ pastar/Node.cpp
@@ -165,7 +165,7 @@ int Node<N>::getNeigh(std::vector<Node> a[], int vec_size)
        third field, sequence2.
        Example, if sequence 0 and 2 matches, one of the vector fields
        contains first MATCH score, second 0, third 2. */
-    std::vector< std::vector<int> > pairwise_costs[(N-1)*2];
+    int pairwise_costs[(N-1)*2*3];
 
     int k = 0;
     for (int i = 0; i < N - 1; i++)
@@ -173,9 +173,10 @@ int Node<N>::getNeigh(std::vector<Node> a[], int vec_size)
         for (int j = i + 1; j < N; j++)
         {
             int cost = Cost::cost(seq->get_seq(i)[pos[i]], seq->get_seq(j)[pos[j]]);
-            std::vector<int>total_cost = {cost, i, j};
-            pairwise_costs[k] = total_cost;
-            k++;
+			pairwise_costs[k] = cost;
+			pairwise_costs[k+1] = i;
+			pairwise_costs[k+2] = j;
+            k=k+3;
         }
     }
 
@@ -186,9 +187,9 @@ int Node<N>::getNeigh(std::vector<Node> a[], int vec_size)
         if (borderCheck(c))
         { 
             int costs = 0; // match, mismatch and gap sum-of-pairs cost
-            for (unsigned int k = 0; k < pairwise_costs.size(); k++)
+            for (unsigned int k = 0; k < ((N - 1) * 2 * 3); k=k+3)
             {
-                costs += pairCost(i,pairwise_costs[k][0], pairwise_costs[k][1], pairwise_costs[k][2]);   
+                costs += pairCost(i,pairwise_costs[k], pairwise_costs[k+1], pairwise_costs[k+2]);   
             }
 
             //If in main diagonal, keep with the owner thread
